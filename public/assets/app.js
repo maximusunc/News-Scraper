fe0a20ad60184560e7e1a774380a99abdab6f75d
Max Wang <maxwellswang@gmail.com>
2018-01-11 16:15:25 -0500

    added saved db with add and remove routes

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,26 +1,7 @@
-$.getJSON("/articles", function(data) {
-    displayResults(data);
-});
-
 $(".scrape").on("click", function(event) {
     event.preventDefault();
     $.get("/scrape", function(results) {
         displayResults(results);
-    });
-});
-
-$(".save").on("click", function(event) {
-    event.preventDefault();
-    $.ajax({
-        method: "POST",
-        url: "/saved/" + id,
-        data: {
-            title: this.title,
-            link: this.link,
-            description: this.description
-        } 
-    }).then(function(data) {
-        console.log(data);
     });
 });
 
@@ -29,8 +10,79 @@ function displayResults(data) {
         var article = "<div class='row'><div class='card blue-grey darken-1'><div class='card-content white-text'>";
         article += "<a href='" + element.link + "'><h2>" + element.title + "</h2></a>";
         article += "<p>" + element.description + "</p></div>";
-        article += "<div class='card-action'><a class='save' href='/saved'><i class='material-icons left'>save</i>Save Article</a></div>";
+        article += "<div class='card-action'><button class='save' data-id=" + element._id + "><i class='material-icons left'>save</i>Save Article</button></div>";
         article += "</div></div></div>";
         $(".articles").append(article);
     });
-}
+    
+    save();
+};
+
+function displaySaved(data) {
+    if (data.length === 0) {
+        var placeholder = "<div class='row'><div class='card yellow darken-1'><div class='card-content red-text'>";
+        placeholder += "<h3>You don't have any saved articles</h3>";
+        placeholder += "</div></div></div>";
+        $(".articles").append(placeholder);
+    } else {
+        $(data).each(function(i, element) {
+            var article = "<div class='row'><div class='card blue-grey darken-1'><div class='card-content white-text'>";
+            article += "<a href='" + element.link + "'><h2>" + element.title + "</h2></a>";
+            article += "<p>" + element.description + "</p></div>";
+            article += "<div class='card-action'><button class='remove' data-id=" + element._id + "><i class='material-icons left'>delete</i>Remove From Saved</button>";
+            article += "<button class='note' data-id=" + element._id + "><i class='material-icons left'>add</i>Add Note</button></div>";
+            article += "</div></div></div>";
+            $(".articles").append(article);
+        });
+    };
+
+    remove();
+    note();
+};
+
+function save() {
+    $(".save").on("click", function(event) {
+        var id = $(this).data("id");
+        $.ajax({
+            method: "POST",
+            url: "/saved/" + id,
+        }).then(function(data) {
+            console.log(data);
+            $.get("/saved", function(result) {
+                console.log(result);
+            });
+        });
+    });
+};
+
+function remove() {
+    $(".remove").on("click", function(event) {
+        var id = $(this).data("id");
+        $.ajax({
+            method: "DELETE",
+            url: "/remove/" + id,
+        }).then(function(data) {
+            console.log(data);
+            $(".articles").empty();
+            $.get("/savedArticles", function(data) {
+                displaySaved(data);
+            });
+        });
+    });
+};
+
+function note() {
+    $(".note").on("click", function(event) {
+        var id = $(this).data("id");
+        $.ajax({
+            method: "POST",
+            url: "/note/" + id,
+        }).then(function(data) {
+            console.log(data);
+            $(".articles").empty();
+            $.get("/savedArticles", function(data) {
+                displaySaved(data);
+            });
+        });
+    });
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var expressHandlebars = require("express-handlebars");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var request = require("request");
+var path = require("path");
 
 var app = express();
 var db = require("./models");
@@ -14,6 +15,7 @@ app.use(
     })
 );
 app.use(express.static("public"));
+app.set('view engine', 'html');
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
 
@@ -22,14 +24,68 @@ mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
+app.get("/articles", function(req, res) {
+    db.Article.find({}, function(error, found) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send(found);
+        };
+    });
+});
+
+app.post("/saved/:id", function(req, res) {
+    db.Article.findOne({_id: req.params.id}, function(error, found) {
+        if (error) {
+            console.log(error);
+        } else {
+            db.Save.create({
+                title: found.title,
+                link: found.link,
+                description: found.description
+            })
+            .then(function(dbSave) {
+                res.json(dbSave);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        }
+    })
+});
+
 app.get("/saved", function(req, res) {
-    db.Save.create(req.body)
-        .then(function(dbSave) {
-            res.json(dbSave);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
+    res.sendFile(path.join(__dirname, "public/saved.html"));
+});
+
+app.get("/savedArticles", function(req, res) {
+    db.Save.find({}, function(error, found) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send(found);
+        };
+    });
+});
+
+app.delete("/remove/:id", function(req, res) {
+    db.Save.remove({_id: req.params.id}, function(error, result) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send(result);
+        };
+    });
+});
+
+app.post("/addNote/:id", function(req, res) {
+    db.Save.update({_id: req.params.id}, {$push: {note: req.body.note}})
+    .then(function(dbNote) {
+        console.log(dbNote);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
 });
 
 app.get("/scrape", function(req, res) {
@@ -43,23 +99,12 @@ app.get("/scrape", function(req, res) {
             result.description = $(element).find("p").children().text();
             db.Article.create(result)
                 .then(function(dbArticle) {
-                    // console.log(dbArticle);
-                    res.json(dbArticle);
+                    console.log(dbArticle);
                 })
                 .catch(function(err) {
-                    console.log(err);
+                    console.log("test");
                 });
         });
-    });
-});
-
-app.get("/articles", function(req, res) {
-    db.Article.find({}, function(error, found) {
-        if (error) {
-            console.log(error);
-        } else {
-            res.send(found);
-        };
     });
 });
 
